2da75851251cd41a3ca2e2520fedef3479572db1
Nishantha <nishanthamms@gmail.com>
2021-06-05 23:15:50 +0530

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.frontend')
+@section('content')
+
+      
+
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+        <div class="col-md-6 px-0">
+          <h1 class="display-6 font-italic">Welcome to Tab Providing System to Schools</h1>
+          
+        </div>
+      </div>
+
+   
+    
+  
+@endsection
