f473ffefdefa276703c3975fb38b989e740662cd
Nishantha <nishanthamms@gmail.com>
2021-06-27 16:34:06 +0530

    home UI added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,147 @@
-@extends('layouts.frontend')
-@section('content')
 
-      
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/mainstyle.css') }}">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+    <title>TPS | Home</title>
+<div class="row">
+  <div class="col-sm-1"></div>
+  <div class="col-sm-3"><h3><span class="weightblue">TPS</span><span class="weightyellow">.lk</span></h3></div>
+  <div class="col-sm-4"></div>
+  <div class="col-sm-2"></div>    
+  <div class="col-sm-2"></div>
+  
+</div> 
+ <!--Navigation Bar-->
 
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-          <h1 class="display-6 font-italic">Welcome to Tab Providing System to Schools</h1>
+          <nav class="navbar navbar-expand-sm bnav">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
           
+            <div class="collapse navbar-collapse ml-5" id="navbarTogglerDemo03">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                  <a class="nav-link text-white" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white" href="#about">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-white disabled" href="#contact">CONTACT US</a>
+                </li>
+              </ul>
+              <div class="form-inline my-2 my-lg-0">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                    <a class="nav-link text-white" href="{{route('login')}}"><i class="fa fa-sign-in text-white" aria-hidden="true"></i> LOGIN</a>
+                  </li>
+                 
+                 
+                </ul>
+            </div>
+          </nav>
+     
+<br>
+
+                                                             <!--carousel slide<-->
+
+<div class="container" id="home">
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <!--img src="img/1.jpg" alt="pic1" width="1100" height="500"-->
+      <img src="{{ asset('img/1.jpg') }}" alt="tag" alt="pic2" width="1100" height="500">
+   
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/2.jpg') }}" alt="tag" alt="pic2" width="1100" height="500">
+      <!--img src="img/2.jpg" alt="pic2" width="1100" height="500"-->
+     
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/3.jpg') }}" alt="tag" alt="pic2" width="1100" height="500">
+      <!--img src="img/3.jpg" alt="pic2" width="1100" height="500"-->
+    
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+</div>
+<br>
+                                                            <!--tabbooking-->
+
+
+<br>
+
+<div class="jumbotron" id="about">
+  <h3 class="text-center text-uppercase">About Us</h3>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, voluptate, inventore tempora repellendus harum atque tempore nesciunt porro optio dolorum minima enim nisi iste molestias similique blanditiis quis aperiam. Itaque!.</p>
+
+</div>
+<section id="contact">
+  <div class="container">
+      <h3 class="text-center text-uppercase">contact us</h3>
+      <p class="text-center w-75 m-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum purus at sem ornare sodales. Morbi leo nulla, pharetra vel felis nec, ullamcorper condimentum quam.</p>
+      <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-4 my-5">
+          <div class="card border-0">
+             <div class="card-body text-center">
+               <i class="fa fa-phone fa-5x mb-3" aria-hidden="true"></i>
+               <h4 class="text-uppercase mb-5">call us</h4>
+               <p>+94 11 2 784811</p>
+             </div>
+           </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 my-5">
+          <div class="card border-0">
+             <div class="card-body text-center">
+               <i class="fa fa-map-marker fa-5x mb-3" aria-hidden="true"></i>
+               <h4 class="text-uppercase mb-5">office loaction</h4>
+              <address>"Isurupaya"
+                Pelawatta,
+                Battaramulla.</address>
+             </div>
+           </div>
+        </div>
+       
+        <div class="col-sm-12 col-md-6 col-lg-4 my-5">
+          <div class="card border-0">
+             <div class="card-body text-center">
+               <i class="fa fa-globe fa-5x mb-3" aria-hidden="true"></i>
+               <h4 class="text-uppercase mb-5">email</h4>
+               <p>secretary.education@moe.gov.lk</p>
+             </div>
+           </div>
         </div>
       </div>
-
-   
+  </div>
+</section>
+  <footer class="footer">
+        <h5 class="text-center text-white"> Copyright ©2021 All rights reserved | Developed by Group 12 </h5>
+  </footer>  
+      <script src="{{ asset('js/bootstrap.min.js') }}"></script> 
+  </body>
+   </html>
     
   
-@endsection
